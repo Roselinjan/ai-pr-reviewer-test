f105c5dbddc39103f76a21d5c8ad6fa367bd65ae
Roselin Janice <your-github-roselinjan@gmail.com>
2026-08-23 11:10:45 +0530

    scenario :a clean addition

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import add, multiply
+from main import add, multiply, subtract
 
 
 def test_add():
@@ -6,3 +6,6 @@ def test_add():
 
 def test_multiply():
     assert multiply(2,3) == 6
+
+def test_subtract():
+    assert subtract(5,3) == 2
